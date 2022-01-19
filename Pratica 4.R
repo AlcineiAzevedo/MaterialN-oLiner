727219c6b541c983a9ae65309acffbae0f7593db
AlcineiAzevedo <65502006+AlcineiAzevedo@users.noreply.github.com>
2022-01-19 14:19:56 -0300

    Add files via upload

---
+++ Pratica 4.R
@@ -0,0 +1,28 @@
+#Instalacao do pacote
+install.packages("NonlinearRegression")
+
+#  Instalacao do pacote pelo github se nao for possivel instalar pelo comando anterior.
+# if(!requireNamespace("devtools", quietly = TRUE))install.packages("devtools")
+#   devtools::install_github("AlcineiAzevedo/NonlinearRegression")
+
+
+
+#####################################################
+#Comparacao de modelos
+#####################################################
+remove(list=ls()) #Apagando a memoria do r
+library(NonlinearRegression)   #Ativando o pacote
+
+#Indicando a pasta de trabalho
+setwd("D:/Backup Pendrive/UFMG/Disciplinas/Tópicos Avançados em Estatística experimental/2021/Regressao nl")
+
+
+#Exemplo com dados de experimento em dbc
+DadosDBC=read.table("DadosDBC.txt",h=T)
+?fit_regression
+
+modelo=y~a*x^b/(c^b+x^b)
+chute=list(a=36,b=2,c=30)
+AjusteDBC=fit_regression(Data =DadosDBC,model = modelo,start = chute,design=3)
+plot_regression(AjusteDBC,plot=7,xlabel = "Doses",ylabel = "Altura (cm)")
+
